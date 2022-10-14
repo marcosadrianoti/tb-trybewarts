5e9e08ccfce4d4a0090da5ea232baf028a58c1d0
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-14 17:10:45 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnLogin = document.getElementById('btn-login');
 function loginVerify() {
   const inputEmail = document.getElementById('email').value;
   const inputPassword = document.getElementById('password').value;
-  if (inputEmail === "tryber@teste.com" && inputPassword === "123456") {
+  if (inputEmail === 'tryber@teste.com' && inputPassword === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
