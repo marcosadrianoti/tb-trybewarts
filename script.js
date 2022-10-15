37bf9c10e483efefba22f3f21be9227269ec202c
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-15 13:50:14 -0300

    Requisito 21 - Formulário de avaliação esconde e formulário de dados aparece

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Executa o botão Login
 const btnLogin = document.getElementById('btn-login');
 function loginVerify() {
   const inputEmail = document.getElementById('email').value;
@@ -10,15 +11,15 @@ function loginVerify() {
 }
 btnLogin.addEventListener('click', loginVerify);
 
+// Ativa o botão Enviar
 const checkBoxAgreement = document.getElementById('agreement');
-
+const btnSubmit = document.getElementById('submit-btn');
 function agreement() {
-  const btnSubmit = document.getElementById('submit-btn');
   btnSubmit.removeAttribute('disabled');
 }
-
 checkBoxAgreement.addEventListener('click', agreement);
 
+// Conta os caracteres do comentário
 const inputTextarea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
 const maxCharacters = 500;
@@ -27,5 +28,15 @@ function counterTextarea() {
   count = inputTextarea.value.length;
   counter.innerText = maxCharacters - count;
 }
-
 inputTextarea.addEventListener('input', counterTextarea);
+
+//Executa o botão Enviar
+const formEvaluation = document.getElementById('evaluation-form');
+const formData = document.getElementById('form-data');
+function submit(event) {
+  event.preventDefault();
+  formEvaluation.style.display = 'none';
+  formData.style.display = '';
+}
+
+btnSubmit.addEventListener('click', submit);
